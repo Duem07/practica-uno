858461f874a5fde85ab38eba794ba424fe265af8
Duem07 <Familia Ortega@LAPTOP-QOR5U70Q>
2022-10-18 17:30:04 -0500

    metodo resta tambien

--- practica2/Program.cs
+++ practica2/Program.cs
@@ -11,12 +11,19 @@ namespace practica2
         static void Main(string[] args)
         {
            Console.WriteLine(suma(34, 56));
+            Console.WriteLine(resta(85, 27));
             Console.Read();
         }
 
         static int suma(int num1, int num2)
         {
             int resultado = num1 + num2;
+            return resultado;
+        }
+
+        static int resta(int num1, int num2)
+        {
+            int resultado = num1 - num2;
             return resultado;
         }
     }
