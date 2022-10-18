02340df0544cf2b8aff553ea2f1b9b2c8acff0ab
Duem07 <Familia Ortega@LAPTOP-QOR5U70Q>
2022-10-18 17:26:31 -0500

    metodo suma

--- practica2/Program.cs
+++ practica2/Program.cs
@@ -10,8 +10,14 @@ namespace practica2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hola mundo");
+           Console.WriteLine(suma(34, 56));
             Console.Read();
+        }
+
+        static int suma(int num1, int num2)
+        {
+            int resultado = num1 + num2;
+            return resultado;
         }
     }
 }
